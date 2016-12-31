4018c971d0dd6cca5de681126eedb7c4c425811e
Batuhan UNAL <unal.batuhan@outlook.com>
2016-12-31 17:49:57 +0000

    Add files via upload

---
+++ Single Operators/src/ExampleApp.java
@@ -0,0 +1,18 @@
+
+public class ExampleApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x,y,z,m;
+		x= 10;
+		y=5;
+		z=16;
+		m=++x - --y + z++;
+		System.out.println("z=" +z);
+		System.out.println("y=" +y);
+		System.out.println("x=" +x);
+		System.out.println("m=" +m);
+
+	}
+
+}

f3a3ef55652387e8e3e249a5976c52bd50beb15c
Batuhan UNAL <unal.batuhan@outlook.com>
2016-12-31 16:22:54 +0000

    My Summary
    Önce x ve y’nin o andaki değerleri geçici bellek alanlarına atanmış (4 ve 8), bu değerlerle ifade hesaplanmış (z=12) sonra x’in değeri 1 arttırılmış ve y’nin değeri 1 azaltılmıştır. İfadeyi z= x+++y--­­; şeklinde boşluksuz yazsak da sonuç değişmezdi. Derleyici bu durumda +++’nın mevcut operatörler içinde mümkün olan en uzununu (++) alır ve ifadeyi şu şekilde yorumlardı:
    
    z=(x++) + y––­­ ;
    
    Aynı ifadeyi z=x+++ veya y––- şeklinde yazsak da aynı sonucu elde ederdik. Ön artım ve ön azaltım operatörleri kullanıldığında ise, önce artım (mevcut değere 1 ekleme) veya azaltım (mevcut değerden 1 çıkarma) gerçekleşecek, sonra ifade hesaplanacaktır.

---
+++ Single Operators/src/ExampleProject.java
@@ -0,0 +1,14 @@
+
+public class ExampleProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=4, y=8,z;
+		z= x++ + y--;
+System.out.println("z=" +z);
+System.out.println("x=" +x);
+System.out.println("y=" +y);
+
+	}
+
+}

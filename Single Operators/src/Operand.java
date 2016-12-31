5466b8c26307d1bbce7879799dd95d4101f3d41e
Batuhan UNAL <unal.batuhan@outlook.com>
2016-12-31 16:14:40 +0000

    My Commit
    Bu bölümde negatifini alma (–), pozitifini alma (+), artım (++) ve azaltım (––) operatörlerini inceleyeceğiz. Negatifini alma operatörü (–) tek bir operand üzerinde etkili olduğu için, iki operand ile işleme giren (––) sembolü ile karıştırılmamalıdır.
    
    Bu programda x’in değeri 11 olarak bulunacaktır. –y – ­(­–5)= +5 olarak hesaplanır. 16’yı izleyen çıkarma operatörü (–) ile y’nin önündeki negatifini alma (–) operatörü arasında mutlaka bir boşluk olmalıdır. Aksi halde (–) ifadesi azaltım operatörü olarak algılanacaktır.

---
+++ Single Operators/src/Operand.java
@@ -0,0 +1,12 @@
+
+public class Operand {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x,y = -5;
+		x = 16- -y;
+		System.out.println("x="+x);
+
+	}
+
+}
